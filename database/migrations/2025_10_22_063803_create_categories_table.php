9797b108bfc6af4b0b0bfbe7815f02b14a9a582a
ELROY HANS ANGGADA <elroy.anggada@binus.edu>
2025-10-22 13:38:43 +0700

    sesi 6

--- database/migrations/2025_10_22_063803_create_categories_table.php
+++ database/migrations/2025_10_22_063803_create_categories_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('portfolios', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->text('description');
-            $table->string('image_url');
-            $table->string('link');
         });
     }
 
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('portfolios');
+        Schema::dropIfExists('categories');
     }
 };
